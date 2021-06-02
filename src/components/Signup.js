3e24dd4bc819ed1838bbf36b844c885b8ebdfa05
Olena Abramova <olena.v.abramova@gmail.com>
2021-06-02 13:04:13 +0300

    create TextField

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form } from 'formik';
 
 import * as Yup from 'yup';
+import TextField from './TextField';
 
 function Signup() {
   return (
@@ -18,6 +19,7 @@ function Signup() {
         <div>
           <h1 className="my-4 font-weight-bold .display-4">Sign Up</h1>
           <Form>
+            <TextField label="First Name" name="firstName" type="text" />
             <button className="btn btn-dark mt-3" type="submit">
               Register
             </button>
---
+++ src/components/TextField.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TextField = ({ label, ...props }) => {
+  return (
+    <div>
+      <input type="text"></input>
+    </div>
+  );
+};
+
+export default TextField;
